1734bd5b821b2b4698191778d4675f3fa440697b
helen-caroline <helen.santos@unico.io>
2025-05-24 13:31:08 -0300

    criando as rotas de autenticação para o keycloak

--- src/app.js
+++ src/app.js
@@ -7,7 +7,21 @@ const vendas_router = require('./routers/vendas/router');
 const app = express();
 app.use(express.json());
 
-// Rota
+// Rotas
+app.get('/', (req, res) => {
+    res.status(200).send('Bem-vindo à API de Gestão de Veículos!')
+});
+app.get('/logout', (req, res) => {
+    // Se estiver usando Keycloak, redirecione para a URL de logout
+    const keycloakLogoutUrl = 'http://localhost:8080/realms/postech-fase3/protocol/openid-connect/logout';
+    res.redirect(keycloakLogoutUrl);
+});
+app.get('/callback', (req, res) => {
+    // Exemplo: Processar o callback do provedor de autenticação
+    res.status(200).send('Callback recebido com sucesso!');
+});
+
+// Rotas de aplicação
 app.use('/veiculos', veiculos_router);
 app.use('/usuarios', usuarios_router);
 app.use('/vendas', vendas_router);
