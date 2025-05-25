27ba59aaf5e8a0b555f408f0132ca3798256f577
helen-caroline <helen.santos@unico.io>
2025-05-25 20:48:34 -0300

    adicionando camada keycloak / login, instrospect, logout

--- src/app.js
+++ src/app.js
@@ -1,44 +1,24 @@
 const express = require('express');
-const session = require('express-session'); // Adicionado para gerenciar sessões
-const Keycloak = require('keycloak-connect'); // Middleware do Keycloak
-
-// Configuração do Keycloak
-const memoryStore = new session.MemoryStore();
-const keycloak = new Keycloak({ store: memoryStore });
-
+const { router: keycloakRouter, keycloak } = require("./routers/keycloack/router");
 const veiculos_router = require('./routers/veiculos/router');
 const usuarios_router = require('./routers/usuarios/router');
 const vendas_router = require('./routers/vendas/router');
 
 const app = express();
 app.use(express.json());
-
-// Configuração de sessão
-app.use(session({
-    secret: '123', // Substitua por uma chave segura
-    resave: false,
-    saveUninitialized: true,
-    store: memoryStore
-}));
-
-// Middleware do Keycloak
-app.use(keycloak.middleware());
+app.use(express.urlencoded({ extended: true }));
 
 // Rotas públicas
 app.get('/', (req, res) => {
     res.status(200).send('Bem-vindo à API de Gestão de Veículos!');
 });
 
-app.get('/logout', (req, res) => {
-    const keycloakLogoutUrl = 'http://localhost:8080/realms/postech-fase3/protocol/openid-connect/logout';
-    const redirectUri = 'http://localhost:3000'; // URL para onde o usuário será redirecionado após o logout
-    res.redirect(`${keycloakLogoutUrl}?redirect_uri=${redirectUri}`);
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK' });
 });
 
-app.get('/callback', (req, res) => {
-    // Exemplo: Processar o callback do Keycloak
-    res.status(200).json({ message: 'Callback recebido com sucesso!', query: req.query });
-});
+// Rotas do Keycloak
+app.use('/keycloak', keycloakRouter);
 
 // Rotas protegidas
 app.use('/veiculos', keycloak.protect(), veiculos_router);
